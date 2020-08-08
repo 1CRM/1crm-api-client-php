f799f1744f93d512f43b2dfe4d940098bbf52d40
Travis CI <travis@travis-ci.org>
2020-08-08 02:13:03 +0000

    Deploy code docs to GitHub Pages Travis build: 23
    Commit: a4c70434ccc9e650c7311409695972d310f8a7c0

--- search/pages_1.js
+++ search/pages_1.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['creating_20records',['Creating records',['../data-create.html',1,'Data']]],
   ['create_20an_20account',['Create an account',['../ex-create-account.html',1,'examples']]],
-  ['create_20an_20invoice_20with_20payment',['Create an Invoice with Payment',['../ex-create-quote.html',1,'examples']]],
+  ['create_20an_20invoice_20with_20payment',['Create an Invoice with Payment',['../ex-create-invoice.html',1,'examples']]],
+  ['create_20a_20quote',['Create a Quote',['../ex-create-quote.html',1,'examples']]],
   ['calendar',['calendar',['../md_docs-pages_calendar.html',1,'']]],
   ['create',['create',['../md_docs-pages_data_create.html',1,'']]],
   ['create_2daccount',['create-account',['../md_docs-pages_examples_create-account.html',1,'']]],
